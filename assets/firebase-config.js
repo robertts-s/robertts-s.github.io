bbdc99942eb9e872b85d1b51010f4eb71bbea131
robertts_ <sergiolopezroberts@gmail.com>
2026-06-10 05:49:04 +0200

    Add files via upload

---
+++ assets/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Console > Configuracion del proyecto > Tus apps > Aplicacion web.
+// La configuracion web identifica el proyecto; la seguridad real vive en
+// Authentication, App Check y las reglas de Realtime Database.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLFAGrqYk0ijbD941hedPCoyvh7Am8SxE",
+  authDomain: "antes-de-morir-quiero.firebaseapp.com",
+  databaseURL: "https://antes-de-morir-quiero-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "antes-de-morir-quiero",
+  storageBucket: "antes-de-morir-quiero.firebasestorage.app",
+  messagingSenderId: "596825157231",
+  appId: "1:596825157231:web:def80444fe19651135a79a"
+};
